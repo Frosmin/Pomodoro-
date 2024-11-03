96026029332355f836cde30e60c9f5ac35b1e2fd
Kevin Huayllas <KevinOmega01@gmail.com>
2024-11-03 15:26:05 -0400

    feat: add user schema

--- PomodoroApp/db/index.ts
+++ PomodoroApp/db/index.ts
@@ -2,19 +2,8 @@ import Realm from "realm"
 
 const realmConfig = {
     schema: [], // Make sure this is your current schema
-    schemaVersion: 2, // Increment this version whenever you change your schema
-    migration: (oldRealm: Realm, newRealm: Realm) => {
-      // Handle migration logic
-      if (oldRealm.schemaVersion < 2) {
-        const oldTasks = oldRealm.objects('Task');
-        const newTasks = newRealm.objects('Task');
-  
-        for (let i = 0; i < oldTasks.length; i++) {
-          const oldTask = oldTasks[i];
-          newTasks[i]._id = oldTask._id; // Handle migration if necessary
-        }
-      }
-    },
+    schemaVersion: 1, // Increment this version whenever you change your schema
+    
   };
 
 const realm: Realm = new Realm(realmConfig);
