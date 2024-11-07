60d9bd13126c9bedb0310aa4d7d0e9cc31819790
Lotiel-Dev <karlos817gerceis@gmail.com>
2024-11-07 19:47:23 -0400

    tareas en view, falta scrollbar

--- PomodoroApp/app/(tabs)/index.tsx
+++ PomodoroApp/app/(tabs)/index.tsx
@@ -15,7 +15,7 @@ export default function HomeScreen() {
     
     <>
       <CircularPomodoroTimer />
-      <TaskList />
+      {/* <TaskList /> */}
     </>
     
   );
--- PomodoroApp/components/CircularPomodoroTimer.tsx
+++ PomodoroApp/components/CircularPomodoroTimer.tsx
@@ -7,6 +7,7 @@ import {
   TouchableOpacity,
   Text,
   ImageBackground,
+  ScrollView,
 } from "react-native";
 import { Svg, Circle } from "react-native-svg";
 import { ActionKind } from "@/context/reducer";
@@ -32,10 +33,18 @@ interface Task {
 }
 const [tasks, setTasks] = useState<Task[]>([
   { id: 1, name: 'tarea 1', completedPomodoros: 0, totalPomodoros: 2 },
-  //{ id: 2, name: 'tarea 2', completedPomodoros: 0, totalPomodoros: 6 },
-  //{ id: 3, name: 'tarea 3', completedPomodoros: 0, totalPomodoros: 6 },
-  //{ id: 4, name: 'tarea 4', completedPomodoros: 0, totalPomodoros: 7 },
-  //{ id: 5, name: 'tarea 5', completedPomodoros: 0, totalPomodoros: 3 },
+  { id: 2, name: 'tarea 2', completedPomodoros: 0, totalPomodoros: 6 },
+  { id: 3, name: 'tarea 3', completedPomodoros: 0, totalPomodoros: 6 },
+  { id: 4, name: 'tarea 4', completedPomodoros: 0, totalPomodoros: 7 },
+  { id: 5, name: 'tarea 5', completedPomodoros: 0, totalPomodoros: 3 },
+  { id: 6, name: 'tarea 6', completedPomodoros: 0, totalPomodoros: 3 },
+  //{ id: 7, name: 'tarea 7', completedPomodoros: 0, totalPomodoros: 3 },
+  //{ id: 8, name: 'tarea 8', completedPomodoros: 0, totalPomodoros: 3 },
+  //{ id: 9, name: 'tarea 9', completedPomodoros: 0, totalPomodoros: 3 },
+  //{ id: 10, name: 'tarea 10', completedPomodoros: 0, totalPomodoros: 3 },
+  //{ id: 11, name: 'tarea 11', completedPomodoros: 0, totalPomodoros: 3 },
+  //{ id: 12, name: 'tarea 12', completedPomodoros: 0, totalPomodoros: 3 },
+  //{ id: 13, name: 'tarea 13', completedPomodoros: 0, totalPomodoros: 3 },
 ]);
 
 //----------------------------
@@ -162,7 +171,7 @@ const [tasks, setTasks] = useState<Task[]>([
           </TouchableOpacity>
         </View>
         {/* vista de tareas */}
-        <View>
+        <View style={styles.taskCon}>
             {tasks.map((task) => (
               <View key={task.id} style={styles.taskContainer}>
                 <Text>{task.name}</Text>
@@ -253,13 +262,18 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   taskContainer: {
-    flexDirection: 'column',
+    flexDirection: 'row',
     justifyContent: 'space-between',
     padding: 10,
     marginBottom: 5,
-    backgroundColor: '#f1f1f1',
+    backgroundColor: '#ef6548', //color tareas
     borderRadius: 5,
   },
+  taskCon: {
+    display: "flex",
+    width: "50%",
+    height: "6%",
+  }
 });
 
 export default CircularPomodoroTimer;
