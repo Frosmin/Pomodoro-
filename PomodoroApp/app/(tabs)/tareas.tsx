6fbbe4310530222f16574e451975fd977af1dd79
Kevin Huayllas <KevinOmega01@gmail.com>
2024-11-08 16:05:41 -0400

    feat: reaccion en tiempo real de la base de datos

--- PomodoroApp/app/(tabs)/tareas.tsx
+++ PomodoroApp/app/(tabs)/tareas.tsx
@@ -12,56 +12,68 @@ import taskList_styles from '@/styles/taskList';
 import { useGlobalContext } from '@/context/AppContext';
 import { Task } from '@/db/models/Task';
 
+
+interface NewTask {
+  name: string;
+  estimated_effort: number;
+}
 const tareas = () => {
 
     const [tasks, setTasks] = useState<Task[]>([]);
-    const [newTask, setNewTask] = useState<string>("");
+    const [newTask, setNewTask] = useState<NewTask>({name: "", estimated_effort: 1});
+    const [render, setRender] = useState<boolean>(false);
     const {
         user,
         controllers:{
-            TaskController: {getTasksByList,deleteTask},
+            TaskController: {getTasksByList,deleteTask,addTask},
             ListController: {getMainListID}}} = useGlobalContext();
 
 
 
-          const handleAddTask = () => {
-            console.log("Añadiendo Tarea");
 
-          }
+            
+    const handleAddTask = () => {
+      if(newTask.name.trim() !== ""){
+        console.log("Añadiendo Tarea");
+        addTask({name: newTask.name, estimated_effort: 1, list_id: getMainListID()});
+        setNewTask({name: "", estimated_effort: 1});
+        setRender(!render);
+      }
+      
+    }
     const handleDeleteTask = (taskId: Realm.BSON.ObjectID) => {  
         console.log(taskId.toString(),"Eliminando Tarea");
-        
+        setRender(!render);
         deleteTask(taskId);
     }
 
 
     const getTasks = useCallback(() => {
-        if(user?.tasks)setTasks(getTasksByList(getMainListID()));
-    },[])
+    },[user?.tasks,tasks]);
 
     useEffect(() => {
-      if (user?.tasks) {
+      if (user?.tasks,tasks) {
           // Only update tasks if there's a change in user.tasks
           setTasks(getTasksByList(getMainListID()));
       }
-  }, []); // Watch for changes in user.tasks
+  }, [render]); // Watch for changes in user.tasks
 
   return (
     <View style={taskList_styles.container}>
       <Text style={util_styles.title}>Tareas</Text>
       <View>
-        <TextInput placeholder="Nueva Tarea" />
-        <Button title='Añadir'></Button>
+        <TextInput placeholder="Nueva Tarea" value={newTask.name} onChangeText={(text) => setNewTask({...newTask, name: text})}/>
+        <Button title='Añadir' onPress={handleAddTask}></Button>
       </View>
       <View>
-        {/* {tasks.length > 0 && tasks.map((task) => (
+        {tasks.length > 0 && tasks.map((task) => (
           <View key={task._id.toString()}>
             <Text>{task.name}</Text>
             <Text>{task.real_effort}</Text>
             <Text>{task.estimated_effort}</Text>
             <Button title='Eliminar' onPress={handleDeleteTask.bind(this,task._id)}></Button>
           </View>
-        ))} */}
+        ))}
       </View>
     </View>
   )
