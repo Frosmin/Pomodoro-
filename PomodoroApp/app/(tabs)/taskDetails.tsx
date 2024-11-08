d5823d0cb4468adb0cd5658cd40da5384b7adb68
Frosmin <simonabastomart@gmail.com>
2024-11-08 19:10:34 -0400

    refactor: mejorar la legibilidad y organización del código en componentes de la aplicación Pomodoro

--- PomodoroApp/app/(tabs)/taskDetails.tsx
+++ PomodoroApp/app/(tabs)/taskDetails.tsx
@@ -1,22 +1,40 @@
-import { View, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
-import { useLocalSearchParams } from 'expo-router';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { useLocalSearchParams } from "expo-router";
 import { useState, useEffect } from "react";
 import { useGlobalContext } from "@/context/AppContext";
 import { Task } from "@/db/models/Task";
+import { setStatusBarNetworkActivityIndicatorVisible } from "expo-status-bar";
 
 export default function TaskDetails() {
   const { Taskname } = useLocalSearchParams<{ Taskname: string }>();
-  const { user, controllers: { TaskController: { getTasksByList, updateTask } } } = useGlobalContext();
+  const {
+    user,
+    controllers: {
+      TaskController: { getTasksByList, updateTask },
+    },
+  } = useGlobalContext();
   const [task, setTask] = useState<Task | null>(null);
-  const [editedTask, setEditedTask] = useState({ name: '', estimated_effort: 0 });
+  const [editedTask, setEditedTask] = useState({
+    name: "",
+    estimated_effort: 0,
+  });
 
   useEffect(() => {
     if (user) {
-      const tasks = getTasksByList(user.lists[0]._id); 
-      const selectedTask = tasks.find(t => t.name === Taskname);
+      const tasks = getTasksByList(user.lists[0]._id);
+      const selectedTask = tasks.find((t) => t.name === Taskname);
       setTask(selectedTask || null);
       if (selectedTask) {
-        setEditedTask({ name: selectedTask.name, estimated_effort: selectedTask.estimated_effort });
+        setEditedTask({
+          name: selectedTask.name,
+          estimated_effort: selectedTask.estimated_effort,
+        });
       }
     }
   }, [Taskname, user]);
@@ -39,22 +57,23 @@ export default function TaskDetails() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Task Details</Text>
+      <Text style={styles.subtitle}>Name</Text>
       <TextInput
-        style={styles.input}
+        style={[styles.input, { height: 10, width: "100%" }]}
         value={editedTask.name}
         onChangeText={(text) => setEditedTask({ ...editedTask, name: text })}
         placeholder="Nombre de la Tarea"
       />
+      <Text style={styles.subtitle}>Efort</Text>
       <TextInput
         style={styles.input}
         value={String(editedTask.estimated_effort)}
-        onChangeText={(text) => setEditedTask({ ...editedTask, estimated_effort: Number(text) })}
+        onChangeText={(text) =>
+          setEditedTask({ ...editedTask, estimated_effort: Number(text) })
+        }
         placeholder="Esfuerzo estimado"
         keyboardType="numeric"
       />
-      <TouchableOpacity style={styles.button} onPress={handleSave}>
-        <Text style={styles.buttonText}>Guardar</Text>
-      </TouchableOpacity>
     </View>
   );
 }
@@ -63,20 +82,29 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 20,
-    backgroundColor: '#fee8c8',
+    backgroundColor: "#fee8c8",
   },
   title: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 20,
     marginTop: 20,
+  },
+  projectName: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    marginBottom: 20,
+    marginTop: 25,
   },
   input: {
     flex: 1,
     borderWidth: 1,
     borderColor: "#ccc",
     padding: 10,
-    marginBottom: 20,
+    marginRight: 10,
     borderRadius: 5,
   },
   button: {
@@ -87,6 +115,15 @@ const styles = StyleSheet.create({
   },
   buttonText: {
     color: "white",
-    textAlign: "center",
+    justifyContent: "center",
+  },
+  projectItem: {
+    padding: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+  },
+  subtitle: {
+    fontSize:20,
+    marginBottom: 10,
   },
 });
