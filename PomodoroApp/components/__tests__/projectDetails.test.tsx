765c542a17eabc6859b418324ffa8d1044bd8989
Frosmin <simonabastomart@gmail.com>
2024-11-07 22:52:02 -0400

    renombrar archivo de pruebas de ProjectDetails y actualizar la ruta de importación en el test

--- PomodoroApp/components/__tests__/projectDetails.test.tsx
+++ PomodoroApp/components/__tests__/projectDetails.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react-native';
-import ProjectDetails from './projectDetails';
+import ProjectDetails from '../../app/(tabs)/projectDetails';
 import { useLocalSearchParams } from 'expo-router';
 
 jest.mock('expo-router', () => ({
