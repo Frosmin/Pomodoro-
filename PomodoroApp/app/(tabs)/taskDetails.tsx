4d1e0c4a63d86f72a1238a2e7ae824fd79bab129
Frosmin <simonabastomart@gmail.com>
2024-11-08 20:04:14 -0400

    Refactor TaskDetails component layout and styling for improved UI

--- PomodoroApp/app/(tabs)/taskDetails.tsx
+++ PomodoroApp/app/(tabs)/taskDetails.tsx
@@ -39,6 +39,16 @@ export default function TaskDetails() {
     }
   }, [Taskname, user]);
 
+  //  const handleTaskPress = (Taskname: string) => {
+  //   console.log("Tarea seleccionada:", Taskname);
+  //   router.push({
+  //     pathname: "../taskDetails",
+  //     params: { Taskname },
+  //   });
+  // };
+
+
+
   const handleSave = () => {
     if (task) {
       updateTask(task._id, editedTask);
@@ -57,24 +67,32 @@ export default function TaskDetails() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Task Details</Text>
-      <Text style={styles.subtitle}>Name</Text>
-      <TextInput
-        style={[styles.input, { height: 10, width: "100%" }]}
-        value={editedTask.name}
-        onChangeText={(text) => setEditedTask({ ...editedTask, name: text })}
-        placeholder="Nombre de la Tarea"
-      />
       
-      <Text style={styles.subtitle}>Efort</Text>
-      <TextInput
-        style={styles.input}
-        value={String(editedTask.estimated_effort)}
-        onChangeText={(text) =>
-          setEditedTask({ ...editedTask, estimated_effort: Number(text) })
-        }
-        placeholder="Esfuerzo estimado"
-        keyboardType="numeric"
-      />
+      <View style={styles.container2}>
+        <Text style={styles.subtitle}>Name</Text>
+        <TextInput
+          style={styles.input}
+          value={editedTask.name}
+          onChangeText={(text) => setEditedTask({ ...editedTask, name: text })}
+          placeholder="Nombre de la Tarea"
+        />
+      </View>
+
+      <View style={styles.container2}>
+        <Text style={styles.subtitle}>Effort</Text>
+        <TextInput
+          style={styles.input}
+          value={String(editedTask.estimated_effort)}
+          onChangeText={(text) =>
+            setEditedTask({ ...editedTask, estimated_effort: Number(text) })
+          }
+          placeholder="Esfuerzo estimado"
+          keyboardType="numeric"
+        />
+      </View>
+      {/* <TouchableOpacity style={styles.button} onPress={Save}>
+        <Text style={styles.buttonText}>Guardar</Text>
+      </TouchableOpacity> */}
     </View>
   );
 }
@@ -82,49 +100,33 @@ export default function TaskDetails() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 20,
+    padding: 1,
     backgroundColor: "#fee8c8",
+    width: "100%",
+    color: "black",
+  },
+  container2: {
+    padding: 10,
+    color: "red",
+    width: "80%", // Ajusta el ancho del contenedor
+    alignSelf: "center", // Centra el contenedor
   },
   title: {
     fontSize: 24,
     fontWeight: "bold",
     marginBottom: 20,
-    marginTop: 20,
-  },
-  projectName: {
-    fontSize: 18,
-    marginBottom: 10,
-  },
-  inputContainer: {
-    flexDirection: "row",
-    marginBottom: 20,
-    marginTop: 25,
+    marginTop: 50,
+    textAlign : "center",
   },
   input: {
-    flex: 1,
     borderWidth: 1,
     borderColor: "#ccc",
     padding: 10,
-    marginRight: 10,
     borderRadius: 5,
-  },
-  button: {
-    backgroundColor: "#007AFF",
-    padding: 10,
-    borderRadius: 5,
-    justifyContent: "center",
-  },
-  buttonText: {
-    color: "white",
-    justifyContent: "center",
-  },
-  projectItem: {
-    padding: 15,
-    borderBottomWidth: 1,
-    borderBottomColor: "#ccc",
+    width: "100%", // Ajusta el ancho del TextInput
   },
   subtitle: {
-    fontSize:20,
+    fontSize: 20,
     marginBottom: 10,
   },
 });
