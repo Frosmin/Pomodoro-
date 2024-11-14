cb4b9d15b5b85fff4c6719610d1bdb88e39de6ad
Frosmin <simonabastomart@gmail.com>
2024-11-14 12:07:06 -0400

    feat: actualizar detalles del proyecto para mostrar el estado y pomodoros estimados

--- PomodoroApp/app/(tabs)/projectDetails.tsx
+++ PomodoroApp/app/(tabs)/projectDetails.tsx
@@ -113,8 +113,8 @@ export default function ProjectDetails() {
             onPress={() => handleTaskPress(item.name)}
           >
             <Text>{item.name}</Text>
-            <Text>{item.real_effort}</Text>
-            <Text>{item.estimated_effort}</Text>
+            <Text>Iniciado: {item.status}</Text>
+            <Text>Pomodoros estimados: {item.estimated_effort}</Text>
             <TouchableOpacity
               style={styles.deleteButton}
               onPress={() => handleDeleteTask(item._id)}
